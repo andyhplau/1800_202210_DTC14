b734de457c1645efc8c05d227f31e51e7c4416a2
Andy Lau <andyhplau@gmail.com>
2022-03-03 21:05:47 -0800

    Setup skeleton template

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,5 +1,7 @@
+// The function to load navbars and footer
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#topnavbarPlaceholder').load('./text/topnav.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
+    console.log($('#bottomnavbarPlaceholder').load('./text/bottomnav.html'));
 }
 loadSkeleton();  //invoke the function
