78ddb52e7b18b0b4b1f13c3b734912d5921426e6
Andy Lau <andyhplau@gmail.com>
2022-03-25 01:53:14 -0700

    Make navbars responsive

---
+++ scripts/bottomnav.js
@@ -0,0 +1,37 @@
+var currentUser
+
+// redirect home button to main.html
+function home_button() {
+    window.location.href="../main.html";
+}
+
+// redirect profile button to profile.html
+function profile_button(){
+    window.location.href="../profile.html";
+}
+
+// function show_profile(){
+//     firebase.auth().onAuthStateChanged(user=>{
+
+//         // check if user is signed in
+//         if (user) {
+//             // go to the correct user document by referencing to the user uid
+//             currentUser = db.collection("users").doc(user.uid);
+//             // get the document for current user
+//             currentUser.get();
+//             .then(userDoc =>{
+//                 var userProfilePictureURL = userDoc.data().profile_picture_URL;
+//                 console.log(userProfilePictureURL)
+//                 // replace user profile picture if the URL is not empty
+//                 if (userProfilePictureURL!= null){
+//                     $("#navProfilePicture").attr("src", userProfilePictureURL);
+//                 }
+//             })
+//         }else{
+//             // No user is signed in.
+//             console.log("No user is signed in")
+//         }
+//     })
+// }
+
+// show_profile();
