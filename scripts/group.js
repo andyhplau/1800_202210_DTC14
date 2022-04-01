50de23d01f5cea45ce9d8f015641196125961ab6
Andy Lau <andyhplau@gmail.com>
2022-04-01 01:15:13 -0700

    Write function to populate members in group.html

--- scripts/group.js
+++ scripts/group.js
@@ -1,17 +1,15 @@
 let groupID;
 
-
-
 function go_suggest_page() {
     window.location.href = "suggestion.html?group_id=" + groupID;
 }
 
 function go_vote_page() {
-    window.location.href = "vote_page.html?group_id="+ groupID;
+    window.location.href = "vote_page.html?group_id=" + groupID;
 }
 
 function go_chat_page() {
-    window.location.href = "chat.html?group_id="+ groupID;
+    window.location.href = "chat.html?group_id=" + groupID;
 }
 
 
@@ -19,6 +17,47 @@ function getGroupID() {
     // create a URL object
     let params = new URL(window.location.href);
     groupID = params.searchParams.get("group_id");
-
+    console.log(groupID)
 }
-getGroupID()
+
+function populateMembers() {
+    // go to the correct group using groupID
+    db.collection("Group").where("id", "==", groupID)
+        .get()
+        .then(groupDoc => {
+            // get the group name
+            var groupName = groupDoc.docs[0].data().name;
+            // pass group name to groupName span
+            $("#groupName").html(groupName);
+
+            // get the members in the group
+            var group = groupDoc.docs[0].data().members;
+
+            group.forEach(uid => {
+                // get the user docs
+                var users = db.collection("users").doc(uid)
+                    .get()
+                    .then(userDoc => {
+                        var userName = userDoc.data().name;
+                        var userProfilePictureURL = userDoc.data().profile_picture_URL;
+                        var userRole = userDoc.data().user_role;
+                        console.log(uid, userName, userProfilePictureURL, userRole)
+                        if (userProfilePictureURL == null) {
+                            var userProfilePictureURL = "./images/profile-icon.png"
+                        }
+                        if (userRole == "Local") {
+                            $("#localUsers").append(`<li class="m-3" style="clear: both;"><img class="rounded-circle border border-dark border-1 d-block mx-3" style="width: 1.7rem; float: left;" src=${userProfilePictureURL}>${userName}</li>`)
+                        } else if (userRole == "Tourist") {
+                            $("#touristUsers").append(`<li class="m-3" style="clear: both;"><img class="rounded-circle border border-dark border-1 d-block mx-3" style="width: 1.7rem; float: left;" src=${userProfilePictureURL}>${userName}</li>`)
+                        }
+                    })
+            })
+        })
+}
+
+function setup() {
+    getGroupID();
+    populateMembers();
+}
+
+$(document).ready(setup);
