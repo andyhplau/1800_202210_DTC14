bc5ab04a33af31a843f10f6c548a23120a8969bc
Andy Lau <andyhplau@gmail.com>
2022-04-05 21:44:37 -0700

    Modify topnavbar

---
+++ scripts/topnav.js
@@ -0,0 +1,37 @@
+// populate user profile picture to bottom navbar
+function show_profile() {
+    firebase.auth().onAuthStateChanged(user => {
+
+        // check if user is signed in
+        if (user) {
+            // go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            // get the document for current user
+            currentUser.get()
+                .then(userDoc => {
+                    var userProfilePictureURL = userDoc.data().profile_picture_URL;
+                    console.log(userProfilePictureURL)
+                    // replace user profile picture if the URL is not empty
+                    if (userProfilePictureURL != null) {
+                        $("#navProfilePicture").attr("src", userProfilePictureURL);
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+        }
+    })
+}
+
+// direct to profile.html
+function profile_button() {
+    window.location.href = "../profile.html";
+}
+
+// call the functions inside
+function setup() {
+    show_profile();
+}
+
+// call the setup function when page is ready
+$(document).ready(setup);
