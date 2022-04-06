1f1bd2cded652a5dd22fc814eb23e1e203801efa
Andy Lau <andyhplau@gmail.com>
2022-04-05 22:20:47 -0700

    Modify bottomnavbar and add go back button

--- scripts/bottomnav.js
+++ scripts/bottomnav.js
@@ -1,40 +1,11 @@
 var currentUser
 
-// redirect home button to main.html
+// direct to main.html
 function home_button() {
     window.location.href="../main.html";
 }
 
-// redirect profile button to profile.html
-function profile_button(){
-    window.location.href="../profile.html";
-}
-
-// populate user profile picture to bottom navbar
-function show_profile(){
-    firebase.auth().onAuthStateChanged(user=>{
-
-        // check if user is signed in
-        if (user) {
-            // go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").doc(user.uid);
-            // get the document for current user
-            currentUser.get()
-            .then(userDoc =>{
-                var userProfilePictureURL = userDoc.data().profile_picture_URL;
-                console.log(userProfilePictureURL)
-                // replace user profile picture if the URL is not empty
-                if (userProfilePictureURL!= null){
-                    $("#navProfilePicture").attr("src", userProfilePictureURL);
-                }
-            })
-        }else{
-            // No user is signed in.
-            console.log("No user is signed in")
-        }
-    })
-}
-
+// let the user to logout and redirect to index.html
 function logout() {
     console.log("logging out user");
     firebase.auth().signOut().then(() => {
@@ -43,6 +14,11 @@ function logout() {
       }).catch((error) => {
         // An error happened.
       });
+}
+
+// direct to previous page
+function go_back(){
+    history.back()
 }
 
 show_profile();
